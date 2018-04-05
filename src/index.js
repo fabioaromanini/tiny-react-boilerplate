c6f87610061b153b4b03bf7ce7cfa713fc3980fe
Fabio Romanini <fabioaromanini@gmail.com>
2018-04-05 16:41:17 -0300

    adds jest

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 
-console.log('Hello world!');
-const arr = [ 1, 2, 3, 5 ];
-const arr2 = [ 1, 2, 3, 4 ];
+const test_function = () => {
+	console.log('Hello world!');
+	const arr = [ 1, 2, 3, 5 ];
+	const arr2 = [ 1, 2, 3, 4 ];
 
-arr.forEach((element, index) => {
-	console.log(element)
-});
+	arr.forEach((element, index) => {
+		console.log(element)
+	});
 
-console.log(_.difference(arr, arr2));
+	const answer = _.difference(arr, arr2);
+	console.log(answer);
+	return answer;
+}
+
+test_function();
+
+export default test_function;
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import index from './index';
+
+test('main function test', () => {
+  expect(index()).toEqual([5]);
+});
